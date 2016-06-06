3ae4717adeab0b4710e559662b0289053c8411c1
Kranklyboy <kranklyboy1000@gmail.com>
2016-06-06 15:41:23 +0200

    kruskal added

--- 2016SS/algodat2/ad2/ad2/ss16/pa/KMST.java
+++ 2016SS/algodat2/ad2/ad2/ss16/pa/KMST.java
@@ -8,34 +8,94 @@ import java.util.*;
  */
 public class KMST extends AbstractKMST {
 
-	/**
-	 * Der Konstruktor. Hier ist die richtige Stelle f&uuml;r die
-	 * Initialisierung Ihrer Datenstrukturen.
-	 * 
-	 * @param numNodes
-	 *            Die Anzahl der Knoten
-	 * @param numEdges
-	 *            Die Anzahl der Kanten
-	 * @param edges
-	 *            Die Menge der Kanten
-	 * @param k
-	 *            Die Anzahl der Knoten, die Ihr MST haben soll
-	 */
+    /**
+     * Der Konstruktor. Hier ist die richtige Stelle f&uuml;r die
+     * Initialisierung Ihrer Datenstrukturen.
+     *
+     * @param numNodes
+     * Die Anzahl der Knoten
+     * @param numEdges
+     * Die Anzahl der Kanten
+     * @param edges
+     * Die Menge der Kanten
+     * @param k
+     * Die Anzahl der Knoten, die Ihr MST haben soll
+     */
 
-    private Object[] edges_sorted;
-    private HashSet<Edge> edges = new HashSet<Edge>();
+    private final HashSet<Edge> edges;
+    private List<Edge> edges_sorted;
+    private static int numNodes;
+    private ArrayList<Edge>[] adj = (ArrayList<Edge>[]) new ArrayList[numNodes];
     private static int k;
-    private Integer numNodes;
     private static int lowerBound;
 
 
-	public KMST(Integer numNodes, Integer numEdges, HashSet<Edge> edges, int k) {
-		this.edges = edges;
-	}
+    public KMST(Integer numNodes, Integer numEdges, HashSet<Edge> edges, int k) {
+        this.edges = edges;
+        this.numNodes = numNodes;
+        this.k = k;
+
+    }
+
+    private void prim(int start) {
+        List<Edge> nachbar = new LinkedList<Edge>();
+
+        for (Edge e : edges) {
+            if (e.node1 == start || e.node2 == start) {
+                if (!nachbar.contains(e)) {
+                    nachbar.add(e);
+                }
+            }
+        }
+    }
+
+    private void kruskal() {
+        ArrayList<Edge> mst = new ArrayList<Edge>();
+        int lowerBound = 0;
+
+        for (Edge e : edges_sorted) {
+            if (mst.size() == k-1) {
+                for (Edge v : mst) {
+                    lowerBound += v.weight;
+                }
+
+                this.lowerBound = lowerBound;
+                return;
+            }
+            else {
+                mst.add(e);
+            }
+        }
+    }
 
 
+    @Override
+    public void run() {
 
-	@Override
-	public void run() { }
+        edges_sorted = new ArrayList<>(edges);
+        Collections.sort(edges_sorted);
+        kruskal();
+        System.out.println(lowerBound);
 
+
+        int[][] start_edges = new int[edges.size()][3];
+        int i = 0;
+        for (Edge r : edges_sorted) {
+            start_edges[i][0] = r.node1;
+            start_edges[i][1] = r.node2;
+            start_edges[i][2] = r.weight;
+            i++;
+        }
+
+        System.out.println(start_edges);
+
+
+        for (int v = 0; v < edges.size(); v++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.printf("%5d ", start_edges[v][j]);
+            }
+            System.out.println();
+        }
+
+    }
 }
